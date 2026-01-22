5c1166a74c05c26936c8917dc161f68a6803437c
Seqoi <darikin123497@mail.ru>
2026-01-22 13:51:17 +0300

    Smart lab first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Onboarding.class);
+                startActivity(intent);
+            }
+        },10000);
+    }
+
+    public void onClick(View v){
+        Intent intent = new Intent(MainActivity.this, Onboarding.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/Onboarding.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Onboarding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+    }
+    public void onClick(View v){
+        Intent intent = new Intent(Onboarding.this, LogInActivity.class);
+        startActivity(intent);
+    }
+}
