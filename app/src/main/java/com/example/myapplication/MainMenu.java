3cb9d87ad919050658c5281784d8320a00cac399
Seqoi <darikin123497@mail.ru>
2026-02-26 13:13:54 +0300

    api

--- app/src/main/java/com/example/myapplication/MainMenu.java
+++ app/src/main/java/com/example/myapplication/MainMenu.java
@@ -1,21 +1,25 @@
 package com.example.myapplication;
 
+import static okhttp3.RequestBody.create;
+
 import android.os.Bundle;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 import java.io.IOException;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Headers;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -28,20 +32,68 @@ public class MainMenu extends AppCompatActivity {
         MediaType JSON = MediaType.get("application/json; charset=utf-8");
         RequestBody body = RequestBody.create(JSON, "");
         Request request = new Request.Builder()
-                .url("http://2.nntc.nnov.ru:8900/api/collections/projects/records")
+                .url("http://localhost:8000/user/profile")
                 .get()
                 .build();
 
         client.newCall(request).enqueue(new okhttp3.Callback() {
+
             @Override
-            public void onFailure(@NonNull okhttp3.Call call, IOException e) {}
+            public void onFailure(@NonNull okhttp3.Call call, IOException e) {
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        Toast.makeText(MainMenu.this, String.valueOf(e), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
             @Override
             public void onResponse(@NonNull okhttp3.Call call, @NonNull okhttp3.Response response) throws IOException {
                 assert response.body() != null;
                 String body = response.body().string();
-                Toast.makeText(MainMenu.this, body, Toast.LENGTH_LONG).show();
+                // Inside your background thread or callback method:
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        Toast.makeText(MainMenu.this, body, Toast.LENGTH_LONG).show();
+                    }
+                });
             }
         });
+
+
+
+//        OkHttpClient client = new OkHttpClient();
+//
+//        Request request = new Request.Builder()
+//                .url("http://localhost:8000/user/profile")
+//                .build();
+//
+//        client.newCall(request).enqueue(new Callback() {
+//            @Override
+//            public void onFailure(Call call, IOException e) {
+//                e.printStackTrace();
+//            }
+//
+//            @Override
+//            public void onResponse(Call call, Response response) throws IOException {
+//                Toast.makeText(MainMenu.this, "Hoti shtoto", Toast.LENGTH_LONG).show();
+//                try (ResponseBody responseBody = response.body()) {
+//
+//                    if (!response.isSuccessful()) {
+//                        throw new IOException("Запрос к серверу не был успешен: " +
+//                                response.code() + " " + response.message());
+//                    }
+//
+//                    // пример получения всех заголовков ответа
+//                    Headers responseHeaders = response.headers();
+//                    for (int i = 0, size = responseHeaders.size(); i < size; i++) {
+//                        // вывод заголовков
+//                        Toast.makeText(MainMenu.this, "URAAAA", Toast.LENGTH_LONG).show();
+//                    }
+//                    // вывод тела ответа
+//                    Toast.makeText(MainMenu.this, "Hoti shtoto", Toast.LENGTH_LONG).show();
+//                }
+//            }
+//        });
 
     }
 }
