cd2cc097cff4c96527333764ceeaa7b8f9a24f98
Seqoi <darikin123497@mail.ru>
2026-02-27 16:18:59 +0300

    working POST Registration

--- app/src/main/java/com/example/myapplication/MainMenu.java
+++ app/src/main/java/com/example/myapplication/MainMenu.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import static okhttp3.RequestBody.create;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -23,77 +25,84 @@ import okhttp3.ResponseBody;
 
 public class MainMenu extends AppCompatActivity {
 
+    private String tokenUserVinogradov = "";
+    EditText emailEditText;
+    EditText nameEditText;
+    EditText lastNameEditText;
+    EditText surnameEditText;
+    EditText dateEditText;
+    EditText genderEditText;
+    EditText passwordEditText;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
-        OkHttpClient client = new OkHttpClient();
-        MediaType JSON = MediaType.get("application/json; charset=utf-8");
-        RequestBody body = RequestBody.create(JSON, "");
-        Request request = new Request.Builder()
-                .url("http://localhost:8000/user/profile")
-                .get()
-                .build();
+        emailEditText = findViewById(R.id.editTextTextEmailAddress);
+        nameEditText = findViewById(R.id.editTextTextName);
+        lastNameEditText = findViewById(R.id.editTextTextLastName);
+        surnameEditText = findViewById(R.id.editTextTextSurname);
+        dateEditText = findViewById(R.id.editTextDate);
+        passwordEditText = findViewById(R.id.editTextTextPassword);
+        genderEditText = findViewById(R.id.editTextTextGender);
 
-        client.newCall(request).enqueue(new okhttp3.Callback() {
+    }
+    public void callPostReg(View v){
 
-            @Override
-            public void onFailure(@NonNull okhttp3.Call call, IOException e) {
-                runOnUiThread(new Runnable() {
-                    public void run() {
-                        Toast.makeText(MainMenu.this, String.valueOf(e), Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-            @Override
-            public void onResponse(@NonNull okhttp3.Call call, @NonNull okhttp3.Response response) throws IOException {
-                assert response.body() != null;
-                String body = response.body().string();
-                // Inside your background thread or callback method:
-                runOnUiThread(new Runnable() {
-                    public void run() {
-                        Toast.makeText(MainMenu.this, body, Toast.LENGTH_LONG).show();
-                    }
-                });
-            }
-        });
+            String email = emailEditText.getText().toString().trim();
+            String name = nameEditText.getText().toString();
+            String lastName = lastNameEditText.getText().toString();
+            String surname = surnameEditText.getText().toString();
+            String date = dateEditText.getText().toString();
+            String pass = passwordEditText.getText().toString();
+            String gender = genderEditText.getText().toString();
 
 
+            OkHttpClient client = new OkHttpClient();
+            MediaType JSON = MediaType.get("application/json; charset=utf-8");
+            RequestBody body = RequestBody.create(JSON, "{\n" +
+                    "  \"name\": \""+ name +"\",\n" +
+                    "  \"last_name\": \""+ lastName +"\",\n" +
+                    "  \"surname\": \""+ surname +"\",\n" +
+                    "  \"date_of_birth\": \""+ date +"\",\n" +
+                    "  \"sex\": \""+ gender +"\",\n" +
+                    "  \"email\": \""+ email + "\",\n" +
+                    "  \"password\": \""+ pass +"\"\n" +
+                    "}");
+            Request request = new Request.Builder()
+                    .url("http://192.168.1.43:8000/user/reg/")
+                    .post(body)
+                    .build();
 
-//        OkHttpClient client = new OkHttpClient();
-//
-//        Request request = new Request.Builder()
-//                .url("http://localhost:8000/user/profile")
-//                .build();
-//
-//        client.newCall(request).enqueue(new Callback() {
-//            @Override
-//            public void onFailure(Call call, IOException e) {
-//                e.printStackTrace();
-//            }
-//
-//            @Override
-//            public void onResponse(Call call, Response response) throws IOException {
-//                Toast.makeText(MainMenu.this, "Hoti shtoto", Toast.LENGTH_LONG).show();
-//                try (ResponseBody responseBody = response.body()) {
-//
-//                    if (!response.isSuccessful()) {
-//                        throw new IOException("Запрос к серверу не был успешен: " +
-//                                response.code() + " " + response.message());
-//                    }
-//
-//                    // пример получения всех заголовков ответа
-//                    Headers responseHeaders = response.headers();
-//                    for (int i = 0, size = responseHeaders.size(); i < size; i++) {
-//                        // вывод заголовков
-//                        Toast.makeText(MainMenu.this, "URAAAA", Toast.LENGTH_LONG).show();
-//                    }
-//                    // вывод тела ответа
-//                    Toast.makeText(MainMenu.this, "Hoti shtoto", Toast.LENGTH_LONG).show();
-//                }
-//            }
-//        });
+            client.newCall(request).enqueue(new okhttp3.Callback() {
+
+                @Override
+                public void onFailure(@NonNull okhttp3.Call call, IOException e) {
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            Toast.makeText(MainMenu.this, String.valueOf(e), Toast.LENGTH_SHORT).show();
+                            System.out.println(String.valueOf(e));
+                        }
+                    });
+                }
+
+                @Override
+                public void onResponse(@NonNull okhttp3.Call call, @NonNull okhttp3.Response response) throws IOException {
+                    assert response.body() != null;
+                    String body = response.body().string();
+                    // Inside your background thread or callback method:
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            Toast.makeText(MainMenu.this, body, Toast.LENGTH_LONG).show();
+                            tokenUserVinogradov = body;
+                            System.out.println(body);
+                        }
+                    });
+                }
+            });
 
     }
 }
